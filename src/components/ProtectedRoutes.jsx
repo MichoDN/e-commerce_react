06148e632b252ad12ad3e5eabef192922fdf7a20
MichoDN <michoproyect72@gmail.com>
2022-10-07 10:13:44 -0400

    up side down login validation

--- src/components/ProtectedRoutes.jsx
+++ src/components/ProtectedRoutes.jsx
@@ -3,13 +3,10 @@ import { Navigate, Outlet } from 'react-router-dom';
 
 const ProtectedRoutes = () => {
     const isLogged = useSelector(state => state.isLogged)
-    if (isLogged) {
-        console.log("logged out")
-        return <Navigate to='/login' />
-    } else {
-        console.log("logged in") 
-        return <Outlet />
-    }
+
+    
+    if (isLogged) return <Outlet />
+    else return <Navigate to='/login' />
 };
 
 export default ProtectedRoutes;
