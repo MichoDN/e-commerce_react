f2b90d150572b14c97aabe1dae3bea481474fe26
MichoDN <michoproyect72@gmail.com>
2022-10-07 10:15:32 -0400

    too much identation!

--- src/components/ProtectedRoutes.jsx
+++ src/components/ProtectedRoutes.jsx
@@ -4,7 +4,6 @@ import { Navigate, Outlet } from 'react-router-dom';
 const ProtectedRoutes = () => {
     const isLogged = useSelector(state => state.isLogged)
 
-    
     if (isLogged) return <Outlet />
     else return <Navigate to='/login' />
 };
